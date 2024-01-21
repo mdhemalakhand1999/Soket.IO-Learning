28d27ad392dab92f4f7aaa442bc2e5ea30a6959a
mdhemalakhand1999 <hemalrika@gmail.com>
2024-01-21 14:04:31 +0600

    updated

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const http = require('http');
 const app = express();
-const server = http.createServer(app);
-
+const server = require("http").createServer(app);
+const socketID = require("socket.io");
+const io = socketID(server);
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname+'/index.html');
 })
-
-
 
 server.listen(3000, function() {
     console.log('server listening on: 3000');
